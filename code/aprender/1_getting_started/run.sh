3027537703056b286a1ab1c1c84eb37e602920ff
adsoftsito <adsoft@live.com.mx>
2023-02-20 01:41:44 +0000

    update reamde

--- code/aprender/1_getting_started/run.sh
+++ code/aprender/1_getting_started/run.sh
@@ -1,9 +1,9 @@
 export APP_NAME=/workspaces/bigdata-spark/code/aprender/1_getting_started/hello_spark.py
 echo "submitting : $APP_NAME ..."
 
-kubectl cp "$APP_NAME" spark-cluster-master-0:/tmp/program.py
-kubectl exec -ti --namespace default spark-cluster-master-0 -- ls /tmp
+kubectl cp "$APP_NAME" my-release-spark-master-0:/tmp/program.py
+kubectl exec -ti --namespace default my-release-spark-master-0 -- ls /tmp
 
-kubectl exec -ti --namespace default spark-cluster-master-0 -- spark-submit \
-    --master spark://spark-cluster-master-svc:7077 \
+kubectl exec -ti --namespace default my-release-spark-master-0 -- spark-submit \
+    --master spark://my-release-spark-master-svc:7077 \
     /tmp/program.py
