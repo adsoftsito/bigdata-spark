4747e6e1cdf71ee1cdf439aa41e452f39a9b2fa0
CodigyX <erickbg8919@gmail.com>
2023-03-03 18:12:59 -0600

    add octave programs

---
+++ parcial1/octave/Suma_diagonal.m
@@ -0,0 +1,22 @@
+A = [0, 1, 2;
+     3, 4, 5;
+     6, 7, 8];
+
+renglones = 3;
+columnas = 3;
+traza=0;
+
+% impresion de matrix con for
+
+for ren=1:renglones;
+  for col=1:columnas;
+    fprintf('%d \t', A(ren,col));
+    if (col==ren)
+      diagonal = A(ren,col);
+      traza = traza + A(ren,col);
+    endif
+      endfor
+
+       fprintf('\n')
+endfor
+        fprintf('Traza : %d', traza);
